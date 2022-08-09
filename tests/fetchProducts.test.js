53f5ed6ed2afeb37272f5fceb53f3ec0c265ff9d
Daniel Wolter <wolter.daniel@gmail.com>
2022-08-09 16:53:20 -0300

    requisito 1 ok

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -2,8 +2,34 @@ require('../mocks/fetchSimulator');
 const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
-describe('1 - Teste a função fetchProducts', () => {
-  // implemente seus testes aqui
+describe('Teste a função fetchProducts', () => {
+  it('Teste se fetchProducts é uma função', () => {
+    expect(typeof fetchProducts).toBe('function');
+    expect.assertions(1);
+  })
+  it('Execute a função fetchProducts com o argumento "computador" e teste se fetch foi chamada', async () => {
+    // await fetchProducts('computador');
+    // expect(fetch).toHaveBeenCalled();
+    await expect(fetchProducts('computador')).toBeenCalled()
+    expect.assertions(1);
+  })
+  it('Teste se, ao chamar a função fetchProducts com o argumento computador, a função fetch utiliza o endpoint', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toBeCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+    expect.assertions(1);
+  })
+  it('o retorno da função fetchProducts com o argumento computador é igual ao objeto computadorSearch', async () => {
+    await expect(fetchProducts('computador')).toEqual(computadorSearch);
+    expect.assertions(1);
+  })
   
-  fail('Teste vazio');
+
+test('Teste se, ao chamar a função fetchProducts sem argumento, retorna um erro com a mensagem:', async () => {
+  expect.assertions(1);
+  try {
+    await fetchProducts();
+  } catch (err) {
+    expect(err).toEqual(new Error('You must provide an url'));
+  }
+})
 });
